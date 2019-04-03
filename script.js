0f0cf9dda1f82b03efc62ad7f1e98a4ffb260f3e
Stacy Gaplevski <gaplevski@gmail.com>
2019-04-03 23:54:35 +0300

    Fixed navbar for mobile, javaskript, styles, mobile

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
 // Look for .hamburger
 var hamburger = document.querySelector('.hamburger');
+// Look for #mainMenu
+var mainMenu = document.querySelector('#main-menu');
 // On click
 hamburger.addEventListener('click', function() {
     // Toggle class "is-active"
     hamburger.classList.toggle('is-active');
-    // Do something else, like open/close menu
+    // Toggle open/close menu
+    mainMenu.classList.toggle("nav-open")
+
+});
+
+var mainMenuItems = document.querySelectorAll('#main-menu li a');
+
+mainMenuItems.forEach(function (item) {
+    item.addEventListener('click', function() {
+        //If this is mobile view, so the hamburger is visible
+        if(hamburger.classList.contains('is-active')){
+            // Toggle class "is-active"
+            hamburger.classList.toggle('is-active');
+            // Toggle open/close menu
+            mainMenu.classList.toggle("nav-open")
+        }
+
+
+    });
 });
